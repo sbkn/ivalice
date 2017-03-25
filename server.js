7f4a42702894a69010cd30df7998e19e0ad1c456
sbkn <sergeybakin87@googlemail.com>
2017-03-25 12:41:12 +0100

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+var webpack = require("webpack");
+var path = require("path");
+var WebpackDevServer = require("webpack-dev-server");
+var config = require("./webpack.config");
+
+const PORT = 3000;
+
+var server = new WebpackDevServer(webpack(config), {
+	publicPath: config.output.publicPath
+});
+
+server.use("/", function (req, res) {
+	res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+server.listen(PORT, "0.0.0.0", function (err, result) {
+
+	if (err) {
+		console.log(err);
+	}
+
+	console.log("Running at http://0.0.0.0:" + PORT);
+});
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+require("../styles/application.scss");
+
+import React from "react";
+import ReactDOM from "react-dom";
+import TestComponent from "./TestComponent/TestComponent.react.jsx";
+
+ReactDOM.render(<TestComponent />, document.getElementById("react-root"));
